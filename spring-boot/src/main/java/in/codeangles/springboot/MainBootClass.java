634853d2f1a1668c3d62780276d67974d5e06406
ashish.bagri@ymail.com <ashish.bagri@ymail.com>
2017-02-09 23:35:55 +0530

    SpringBoot starter application

---
+++ spring-boot/src/main/java/in/codeangles/springboot/MainBootClass.java
@@ -0,0 +1,12 @@
+package in.codeangles.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBootClass {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MainBootClass.class, args);
+	}
+}
